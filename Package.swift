89f3fbb3f0f5239c60420e3ff3b8188a6d56d0eb
Andrii Pylypenko <shadowchamber@icloud.com>
2023-12-23 12:01:25 +0200

    upd

--- Package.swift
+++ Package.swift
@@ -5,19 +5,32 @@ import PackageDescription
 
 let package = Package(
     name: "swift-tca-dependency-reminders",
+    platforms: [
+      .iOS(.v13),
+      .macOS(.v10_15),
+      .tvOS(.v13),
+      .watchOS(.v6),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "swift-tca-dependency-reminders",
-            targets: ["swift-tca-dependency-reminders"]),
+            name: "Reminders",
+            targets: ["Reminders"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swipentap/swift-tca-appl.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swift-tca-dependency-reminders"),
+            name: "Reminders",
+            dependencies: [
+                .product(name: "Appl", package: "swift-tca-appl"),
+            ]
+        ),
         .testTarget(
-            name: "swift-tca-dependency-remindersTests",
-            dependencies: ["swift-tca-dependency-reminders"]),
+            name: "RemindersTests",
+            dependencies: ["Reminders"]),
     ]
 )
